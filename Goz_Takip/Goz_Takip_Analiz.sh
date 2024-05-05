a54229c8f8cbd21e4b542045c0f7e8e20d5785cd
Mehmet Göğebakan <37026650+mehmetgogebakan@users.noreply.github.com>
2024-05-05 17:04:29 +0300

    Create Goz_Takip_Analiz.sh

---
+++ Goz_Takip/Goz_Takip_Analiz.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Metin dosyasının adı
+echo "Analiz Edilecek TXT Metin Dosyası Adını Giriniz:"
+
+#Analiz edilecek dosyayı okuyoruz:
+read METIN_DOSYASI
+
+
+# İkili harf analizi
+starttime1=$(date +"%s")
+
+dizi=($(echo {a..z}{a..z}))
+for i in ${dizi[@]}; do 
+    sayi=$(grep -o $i "${METIN_DOSYASI}" | wc -l)
+    echo "$i;$sayi" >> "${METIN_DOSYASI}_ikili_harf_analizi.txt"
+done
+
+endtime1=$(date +"%s")
+math=$((endtime1-starttime1))
+echo "İkili harf analizi için çalıştığımız script $math saniye sürdü"
+
+# Derece analizi ve toplam derece hesaplama
+starttime2=$(date +"%s")
+
+METIN_ikili_harf_analizi=$(sed '/;0/d' "${METIN_DOSYASI}_ikili_harf_analizi.txt")
+
+while IFS=';' read -r harf_ikilisi kac_kere; do
+    kac_derece=$(grep -w "$harf_ikilisi" sonuclar_derece.txt | cut -d ":" -f 2)
+    sonuc=$(echo "scale=5;$kac_kere*$kac_derece" | bc)
+    echo "$harf_ikilisi;$kac_kere;$kac_derece;$sonuc" >> "${METIN_DOSYASI}_tum_derece_sonuclar.txt"
+done <<< $METIN_ikili_harf_analizi
+
+endtime2=$(date +"%s")
+math=$((endtime2-starttime2))
+echo "İkili harfler için göz açılarının hesaplanması $math saniye sürdü"
+
+toplam=$(cat "${METIN_DOSYASI}_tum_derece_sonuclar.txt" | awk -F ";" '{ topla += $4 } END { printf "%.f\n",topla }')
+
+endtime=$(date +"%s")
+math=$((endtime-starttime))
+echo "Analiz için çalıştığımız script $math saniye sürdü"
+
+
+echo "Göz takibinde metni yazdırmak için gereken göz açısı değişimi $toplam derecedir."
+echo "Toplam;;;$toplam" >> "${METIN_DOSYASI}_tum_derece_sonuclar.txt"
